8a5c7e1a99e4889437043ee2c011516da36b5cad
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2023-01-26 22:16:39 +0530

    darkMode working correctly

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,16 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import React from 'react';
+import React, {useState} from 'react';
 import Textform from './components/Textform';
+import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('dark');
+  // document.body.style.backgroundColor = 'grey';
+  const toggleMode = () => {
+    if (mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey';
+    }
+    
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <div className="App">
       <React.Fragment>
-        <Navbar title="TextUtils" home="Home" about="About Us" />
-        <div class="container my-3"><Textform  heading="Enter your text here"/></div>
+        <Navbar title="TextUtils" home="Home" about="About Us" mode={mode} toggleMode={toggleMode} />
+        <div className="container my-3"><Textform  heading="Enter your text here" mode={mode} /></div>
         
+        {/* <About/> */}
       </React.Fragment>
       
     </div>
